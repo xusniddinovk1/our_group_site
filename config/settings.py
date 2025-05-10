6876165ee6d66d80df4b3eb6fae8422a37392570
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-05-10 16:46:11 +0500

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -87,7 +87,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls'))
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
