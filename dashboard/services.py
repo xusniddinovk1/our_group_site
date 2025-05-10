13f8a9fcb2165e9db4d6fdc7f3698cdfad4180b4
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-05-10 21:47:39 +0500

    update services.py

--- dashboard/services.py
+++ dashboard/services.py
@@ -3,7 +3,7 @@ from contextlib import closing
 
 
 def dict_fetchall(cursor):
-    columns = [col[0] for col in cursor.descrption]
+    columns = [col[0] for col in cursor.description]
     return [
         dict(zip(columns, row)) for row in cursor.fetchall()
     ]
@@ -26,7 +26,7 @@ def get_subjects():
 
 def get_teachers():
     with closing(connection.cursor()) as cursor:
-        cursor.execute("""SELECT students_teacher.id, students_teachers.first_name, students_teacher.last_name, 
+        cursor.execute("""SELECT students_teacher.id, students_teacher.first_name, students_teacher.last_name, 
                         students_teacher.phone_number, students_subject.name as subject_name FROM students_teacher
                         LEFT JOIN students_subject ON students_teacher.subject_id = students_subject.id""")
         teachers = dict_fetchall(cursor)
@@ -43,7 +43,7 @@ def get_students():
 
 def get_failed_credits():
     with closing(connection.cursor()) as cursor:
-        cursor.execute("""SELECT students_studentsubjectfailure.id, students_student.first_name, students_student.last_name
+        cursor.execute("""SELECT students_studentsubjectfailure.id, students_student.first_name, students_student.last_name,
                         students_subject.name, students_studentsubjectfailure.failed_credits FROM students_studentsubjectfailure LEFT JOIN 
                         students_student ON students_studentsubjectfailure.student_id = students_student.id LEFT JOIN 
                         students_subject ON students_studentsubjectfailure.subject_id = students_subject.id""")
