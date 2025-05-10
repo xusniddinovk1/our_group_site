2c136c89fe06748aa68cf33ffa42de900fffbab5
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-05-10 15:15:23 +0500

    create forms.py

---
+++ dashboard/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from students.models import *
+
+
+class SubjectForm(forms.ModelForm):
+    class Meta:
+        model = Subject
+        fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'total_credits': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
+class TeacherForm(forms.ModelForm):
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = '__all__'
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
+class StudentSubjectFailureForm(forms.ModelForm):
+    class Meta:
+        model = StudentSubjectFailure
+        fields = '__all__'
+        widgets = {
+            'student': forms.Select(attrs={'class': 'form-control'}),
+            'subject': forms.Select(attrs={'class': 'form-control'}),
+            'phone_number': forms.IntegerField(attrs={'class': 'form-control'}),
+        }
---
+++ dashboard/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+
+
+urlpatterns = [
+    path('', main_dashboard, name='main_dashboard')
+]
